5532a638bae8043382d2dd688572fe236b7ac77f
Gourav More <gourav_m@tekditechnologies.com>
2021-06-11 10:56:39 +0530

    Issue #0000 fix: Angular v9 updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { CommonFormElementsModule } from 'common-form-elements';
+import { CommonFormElementsModule } from 'common-form-elements';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-  //  CommonFormElementsModule,
+    CommonFormElementsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
