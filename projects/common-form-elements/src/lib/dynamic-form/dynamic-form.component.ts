6b51397cc139acbcd2773814aeaeda8b0a7c3502
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2021-07-27 23:22:35 +0530

    #SB-25556 feat: Support for publish checklist in generalized collection editor

--- projects/common-form-elements/src/lib/dynamic-form/dynamic-form.component.ts
+++ projects/common-form-elements/src/lib/dynamic-form/dynamic-form.component.ts
@@ -359,4 +359,11 @@ export class DynamicFormComponent implements OnInit, OnChanges, OnDestroy  {
     return null;
     // return result ? {compare: true} : null;
   }
+  checkSectionLevelCss(section) {
+    if (_.has(section, 'renderingHints.class')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
