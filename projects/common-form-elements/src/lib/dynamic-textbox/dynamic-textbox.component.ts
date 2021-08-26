0bd16130ef658a283342e2e9787cfef668bb9b7f
rejneesh1 <pillairejneesh@gmail.com>
2021-08-26 22:52:20 +0530

    Issue SB-0000:feat added depends check on textbox

--- projects/common-form-elements/src/lib/dynamic-textbox/dynamic-textbox.component.ts
+++ projects/common-form-elements/src/lib/dynamic-textbox/dynamic-textbox.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit, AfterViewInit, OnChanges, ViewChild, ElementRef} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { FieldConfigAsyncValidation } from '../common-form-config';
+import * as _ from 'lodash-es';
+import { merge, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators'
 
 @Component({
   selector: 'sb-dynamic-textbox',
@@ -20,11 +23,17 @@ export class DynamicTextboxComponent implements OnInit,  AfterViewInit, OnChange
   @Input() field?: any;
   @Input() disabled: Boolean;
   @ViewChild('validationTrigger') validationTrigger: ElementRef;
+  @Input() depends?: FormControl[];
+  public isTextBoxRequired: String='yes';
+  contextValueChangesSubscription?: Subscription;
 
   constructor() {
   }
 
   ngOnInit() {
+    if (!_.isEmpty(this.depends)) {
+      this.handleDependantFieldChanges();
+    }
   }
 
   ngOnChanges() {
@@ -41,6 +50,15 @@ export class DynamicTextboxComponent implements OnInit,  AfterViewInit, OnChange
         this.validationTrigger.nativeElement
       ));
     }
+  }
+
+  handleDependantFieldChanges() {
+    this.contextValueChangesSubscription =  merge(..._.map(this.depends, depend => depend.valueChanges)).pipe(
+      tap((value: any) => {
+        this.isTextBoxRequired = _.toLower(value);
+      })
+      ).subscribe();
+      this.isTextBoxRequired = _.toLower(_.first(_.map(this.depends, depend => depend.value)));
   }
 
 }
