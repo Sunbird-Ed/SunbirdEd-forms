d7496aa0602bdf2e4f356fe9159247f504741f9b
Venkateshwaran Selvaraj <venkat@sanketika.in>
2021-10-21 13:39:52 +0530

    Issue #SB-21820 fix: dependency for date component in sb forms

--- projects/common-form-elements/src/lib/dynamic-date/dynamic-date.component.ts
+++ projects/common-form-elements/src/lib/dynamic-date/dynamic-date.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit, AfterViewInit, OnChanges, ViewChild, ElementRef} from '@angular/core';
+import {Component, Input, OnInit, AfterViewInit, OnChanges, ViewChild, ElementRef, OnDestroy} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { FieldConfigAsyncValidation } from '../common-form-config';
 import { DatePipe } from '@angular/common';
+import * as _ from 'lodash-es';
 import { tap } from 'rxjs/operators';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { merge, Subscription } from 'rxjs';
 import * as moment_ from 'moment';
 
 @Component({
@@ -11,7 +12,7 @@ import * as moment_ from 'moment';
   templateUrl: './dynamic-date.component.html',
   styleUrls: ['./dynamic-date.component.css']
 })
-export class DynamicDateComponent implements OnInit {
+export class DynamicDateComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() asyncValidation?: FieldConfigAsyncValidation;
   @Input() label: String;
   @Input() labelHtml: any;
@@ -22,15 +23,25 @@ export class DynamicDateComponent implements OnInit {
   @Input() default: String;
   @Input() field?: any;
   @Input() disabled: Boolean;
+  @Input() depends?: FormControl[];
   @ViewChild('validationTrigger') validationTrigger: ElementRef;
   valueChangesSubscription: Subscription;
+  contextValueChangesSubscription: any;
+  shouldBeVisible: any = 'yes';
   constructor() {
   }
 
   ngOnInit() {
-    let result = this.validations.find(data => data.type==='dateFormat');
-    let date = moment_(this.field.default,result.value).format("YYYY-MM-DD")
-    this.formControlRef.setValue(date);
+    const result = this.validations.find(data => data.type === 'dateFormat');
+
+    if (this.default) {
+      const date = moment_(this.field.default, result.value).format('YYYY-MM-DD');
+      this.formControlRef.setValue(date);
+    }
+
+    if (!_.isEmpty(this.depends)) {
+      this.handleDependantFieldChanges();
+    }
   }
 
   ngAfterViewInit() {
@@ -44,6 +55,21 @@ export class DynamicDateComponent implements OnInit {
         this.validationTrigger.nativeElement
       ));
       console.log(this.formControlRef);
+    }
+  }
+
+  handleDependantFieldChanges() {
+    this.contextValueChangesSubscription =  merge(..._.map(this.depends, depend => depend.valueChanges)).pipe(
+      tap((value: any) => {
+        this.shouldBeVisible = _.toLower(value);
+      })
+      ).subscribe();
+      this.shouldBeVisible = _.toLower(_.first(_.map(this.depends, depend => depend.value)));
+  }
+
+  ngOnDestroy(): void {
+    if (this.contextValueChangesSubscription) {
+      this.contextValueChangesSubscription.unsubscribe();
     }
   }
 }
